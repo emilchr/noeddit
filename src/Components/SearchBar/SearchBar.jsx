73cf68b2c73a59f6e9605f732a2eb21235d8ff12
emilchr <98517843+emilchr@users.noreply.github.com01>
2024-03-01 19:34:04 +0100

    Search-modal done.

--- src/Components/SearchBar/SearchBar.jsx
+++ src/Components/SearchBar/SearchBar.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import SearchRoundedIcon from '@mui/icons-material/SearchRounded';
+import CloseRoundedIcon from '@mui/icons-material/CloseRounded';
 import './SearchBar.css';
 
 export const SearchBar = () => {
   return (
     <form>
         <div className='search-modal'>
-          <input type="search" name="search" id="search" />
+          <div className="search-modal-top"><CloseRoundedIcon /></div>
+          <input type="search" name="search" id="search" placeholder='Search...' />
         </div>
         <button type="submit"><SearchRoundedIcon style={{color: 'white'}}/></button>
     </form>
