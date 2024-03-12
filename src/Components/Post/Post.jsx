98cd0b694e9e7458731dc0f51de305dbcb52072b
emilchr <98517843+emilchr@users.noreply.github.com01>
2024-03-12 13:34:13 +0100

    Refactored Post and PostList

--- src/Components/Post/Post.jsx
+++ src/Components/Post/Post.jsx
@@ -1,44 +1,27 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
-import { loadAllPhotos, loadAllPosts } from '../../Features/Posts/postsSlice';
 import './Post.css';
 
-export const Post = () => {
-	const loadPosts = useSelector(loadAllPosts);
-	const loadPhotos = useSelector(loadAllPhotos);
+export const Post = (props) => {
+	
+	return <><div className="post" id={props.pId} >
+	<div className="sub-title">r/facePalm</div>
+	<h2 className="post-title">Post title: {props.title}</h2>
 
-	const photos = loadPhotos.map((photo) => {
-		// loads all photos into the variable photos and returns a img and title.
-		return <img src={photo.thumbnailUrl} alt={photo.title}></img>;
-	});
-
-	const posts = loadPosts.map((post, index) => {
-		// loads all posts and combines photos with the post.
-		return (
-			<div className="post" id={post.id} key={post.id}>
-				<div className="sub-title">r/facePalm</div>
-				<h2 className="post-title">Post title: {post.title}</h2>
-
-				<div className="votes">
-					Up
-					<br />
-					2.7k
-					<br />
-					Down
-				</div>
-				<div className="post-text">
-					<p>Body: {post.body}</p>
-				</div>
-				<div className="image-container">
-					{
-						photos[index] // inserts a photo in each post.
-					}
-				</div>
-				<div className="post-info">
-					<p>Posted by userID: {post.userId} - 2 minutes ago - Comments</p>
-				</div>
-			</div>
-		);
-	}).splice(0, 10); // Slice added for dev to max posts
-	return <>{posts}</>;
+	<div className="votes">
+		Up
+		<br />
+		2.7k
+		<br />
+		Down
+	</div>
+	<div className="post-text">
+		<p>Body: {props.body}</p>
+	</div>
+	<div className="image-container">
+		{props.photo}
+	</div>
+	<div className="post-info">
+		<p>Posted by userID: {props.userId} - 2 minutes ago - Comments</p>
+	</div>
+</div></>;
 };
--- src/Components/PostList/PostList.jsx
+++ src/Components/PostList/PostList.jsx
@@ -1,14 +1,37 @@
 import React from 'react';
 import { Post } from '../Post/Post';
 import './PostList.css';
+import { useSelector } from 'react-redux';
+import { loadAllPosts, loadAllPhotos } from '../../Features/Posts/postsSlice';
+
 
 export const PostList = () => {
+  
+  const loadPhotos = useSelector(loadAllPhotos);
+  
+  const loadPosts = useSelector(loadAllPosts);
+  const listPosts = loadPosts.map((post) => {
 
-    
+    const photos = loadPhotos.map((photo) => {
+      // loads all photos into the variable photos and returns a img and title.
+      return <img key={photo.id} src={photo.thumbnailUrl} alt={photo.title}></img>;
+    });
+
+    return(
+    <Post 
+        pId={post.id} 
+        title={post.title}
+        body={post.body}
+        photo={photos[1]}
+        userId={post.userId}
+        key={post.id}
+      />
+    )
+  }); 
 
   return (
-    <div className='postList'>
-        <Post />
-    </div>
+    <>
+    {listPosts}      
+    </>
   )
 }
